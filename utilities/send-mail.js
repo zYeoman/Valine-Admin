177cedc98b147b8c349810f1a767bfdc417ce999
zhaojun1998 <35387985+zhaojun1998@users.noreply.github.com>
2018-01-14 18:41:54 +0800

    美化给站长的邮件通知

--- utilities/send-mail.js
+++ utilities/send-mail.js
@@ -12,14 +12,19 @@ const transporter = nodemailer.createTransport({
 
 exports.notice = (comment) => {
     let emailSubject = '👉 咚！「' + process.env.SITE_NAME + '」上有新评论了';
-    let emailContent = '<p>「' + process.env.SITE_NAME + '」上 '
+    let emailContent = '<div style="background-color:white;border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;width:500px;margin:50px auto;color:#555555;font-family:\'Century Gothic\',\'Trebuchet MS\',\'Hiragino Sans GB\',微软雅黑,\'Microsoft Yahei\',Tahoma,Helvetica,Arial,\'SimSun\',sans-serif;font-size:12px;">  \n' +
+        '    <h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;"><span style="color: #12ADDB;font-weight: bold;">&gt; </span>「'
+        + process.env.SITE_NAME
+        + '」上有一条新评论，内容如下：<div style="padding:0 12px 0 12px;margin-top:18px"><p><strong>'
         + comment.get('nick')
-        +' 留下了新评论，内容如下：</p>'
+        + '</strong>&nbsp;回复说：</p><p style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0">'
         + comment.get('comment')
-        + '<br><p> <a href="'
+        + '</p><p><a style="text-decoration:none; color:#12addb" href="'
         + process.env.SITE_URL
         + comment.get('url')
-        + '">点击前往查看</a>';
+        + '" target="_blank">点击前往查看</a></p>  \n' +
+        '    </div>  \n' +
+        '</div>';
 
     let mailOptions = {
         from: '"' + process.env.SENDER_NAME + '" <' + process.env.SENDER_EMAIL + '>',
@@ -41,7 +46,7 @@ exports.send = (currentComment, parentComment)=> {
         '    <h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;"><span style="color: #12ADDB;font-weight: bold;">&gt; \n' +
         '    </span>您('
         + parentComment.get('nick')
-        + ')在<a style="text-decoration:none;color: #12ADDB;" href="' + currentComment.get('url') + '" target="_blank">' + process.env.SITE_NAME + '</a>上的评论有了新的回复</h2> '
+        + ')在<a style="text-decoration:none;color: #12ADDB;" href="'  + process.env.SITE_URL+ currentComment.get('url') + '" target="_blank">《' + process.env.SITE_NAME + '》</a>上的评论有了新的回复</h2> '
         + '你的评论'
         + '<div style="padding:0 12px 0 12px;margin-top:18px"><p>你的评论：</p><p style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0">'
         + parentComment.get('comment')
@@ -50,7 +55,9 @@ exports.send = (currentComment, parentComment)=> {
         +'</strong>&nbsp;回复说：</p><p style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0">'
         + currentComment.get('comment')
         + '</p>'
-        + '<p>您可以点击 <a style="text-decoration:none; color:#12addb" href="' + currentComment.get('url') + '" target="_blank">查看回复的完整內容 </a>。欢迎再次光临 <a style="text-decoration:none; color:#12addb" href="' + process.env.SITE_URL +'" target="_blank">' + process.env.SITE_NAME +'</a>。<br>本邮件为系统自动发送，请勿直接回复</p>  \n' +
+        + '<p>您可以点击 <a style="text-decoration:none; color:#12addb" href="'
+        + process.env.SITE_URL + currentComment.get('url')
+        + '" target="_blank">查看回复的完整內容 </a>，欢迎再次光临 <a style="text-decoration:none; color:#12addb" href="' + process.env.SITE_URL +'" target="_blank">' + process.env.SITE_NAME +'</a>。<br>本邮件为系统自动发送，请勿直接回复。</p>  \n' +
         '    </div>  \n' +
         '</div>';
 
