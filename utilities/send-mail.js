89739b520f3e09e9afc8ed7fb91365ac1a2e0f6d
赵俊 <github@jun6.net>
2018-04-21 11:34:44 +0800

    优化邮件模板
    兼容 Valine 自动生成时自带一个 <p> 标签的状况，原来的写法是 <p>评论内容</p>,生成的结果就是两个行内元素嵌套了，会出现一些问题。所以讲外层的 p 标签改为 div ，并设置自动换行。

--- utilities/send-mail.js
+++ utilities/send-mail.js
@@ -10,6 +10,7 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+// 提醒站长
 exports.notice = (comment) => {
     let emailSubject = '👉 咚！「' + process.env.SITE_NAME + '」上有新评论了';
     let emailContent = '<div style="background-color:white;border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;width:500px;margin:50px auto;color:#555555;font-family:\'Century Gothic\',\'Trebuchet MS\',\'Hiragino Sans GB\',微软雅黑,\'Microsoft Yahei\',Tahoma,Helvetica,Arial,\'SimSun\',sans-serif;font-size:12px;">  \n' +
@@ -17,9 +18,9 @@ exports.notice = (comment) => {
         + process.env.SITE_NAME
         + '」上有一条新评论，内容如下：<div style="padding:0 12px 0 12px;margin-top:18px"><p><strong>'
         + comment.get('nick')
-        + '</strong>&nbsp;回复说：</p><p style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0">'
+        + '</strong>&nbsp;回复说：</p><div style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;">'
         + comment.get('comment')
-        + '</p><p><a style="text-decoration:none; color:#12addb" href="'
+        + '</div><p><a style="text-decoration:none; color:#12addb" href="'
         + process.env.SITE_URL
         + comment.get('url')
         + '" target="_blank">点击前往查看</a></p>  \n' +
@@ -40,21 +41,22 @@ exports.notice = (comment) => {
     });
 }
 
+// 发送邮件通知他人
 exports.send = (currentComment, parentComment)=> {
     let emailSubject = '👉 叮咚！「' + process.env.SITE_NAME + '」上有人@了你';
-    let emailContent = '<div style="background-color:white;border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;width:500px;margin:50px auto;color:#555555;font-family:\'Century Gothic\',\'Trebuchet MS\',\'Hiragino Sans GB\',微软雅黑,\'Microsoft Yahei\',Tahoma,Helvetica,Arial,\'SimSun\',sans-serif;font-size:12px;">  \n' +
+    let emailContent = '<div style="border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;width:500px;margin:50px auto;font-size:12px;">  \n' +
         '    <h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;"><span style="color: #12ADDB;font-weight: bold;">&gt; \n' +
         '    </span>您('
         + parentComment.get('nick')
         + ')在<a style="text-decoration:none;color: #12ADDB;" href="'  + process.env.SITE_URL+ currentComment.get('url') + '" target="_blank">《' + process.env.SITE_NAME + '》</a>上的评论有了新的回复</h2> '
         + '你的评论'
-        + '<div style="padding:0 12px 0 12px;margin-top:18px"><p>你的评论：</p><p style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0">'
+        + '<div style="padding:0 12px 0 12px;margin-top:18px"><p>你的评论：</p><div style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;">'
         + parentComment.get('comment')
-        + '</p><p><strong>'
+        + '</div><p><strong>'
         + currentComment.get('nick')
-        +'</strong>&nbsp;回复说：</p><p style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0">'
+        +'</strong>&nbsp;回复说：</p><div style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;">'
         + currentComment.get('comment')
-        + '</p>'
+        + '</div>'
         + '<p>您可以点击 <a style="text-decoration:none; color:#12addb" href="'
         + process.env.SITE_URL + currentComment.get('url')
         + '" target="_blank">查看回复的完整內容 </a>，欢迎再次光临 <a style="text-decoration:none; color:#12addb" href="' + process.env.SITE_URL +'" target="_blank">' + process.env.SITE_NAME +'</a>。<br>本邮件为系统自动发送，请勿直接回复。</p>  \n' +
@@ -72,7 +74,7 @@ exports.send = (currentComment, parentComment)=> {
         if (error) {
             return console.log(error);
         }
-        console.log('邮件 %s 成功发送: %s', info.messageId, info.response);
+        console.log('邮件成功发送: %s', info.messageId, info.response);
         currentComment.set('isNotified', true);
         currentComment.save();
     });
