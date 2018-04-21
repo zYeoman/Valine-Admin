8b80df7d4bf864211e76e36ee1fc46c1a91be1ca
赵俊 <github@jun6.net>
2018-04-21 15:06:50 +0800

    优化邮件逻辑

--- cloud.js
+++ cloud.js
@@ -8,7 +8,7 @@ AV.Cloud.afterSave('Comment', function (request) {
 
     // 发送博主通知邮件
     mail.notice(currentComment);
-    console.log("已提醒站长");
+    console.log("收到一条评论, 已提醒站长");
     
     // AT评论通知
     let rid;
@@ -22,7 +22,7 @@ AV.Cloud.afterSave('Comment', function (request) {
         let end = comment.substr(start).indexOf("'");
         rid = comment.substr(start, end);
     } else {
-        console.log("没有 @ 任何人");
+        console.log("这条评论没有 @ 任何人");
         return;
     }
     
@@ -31,9 +31,9 @@ AV.Cloud.afterSave('Comment', function (request) {
     query.get(rid).then(function (parentComment) {
         if (parentComment.get('mail')) {
             mail.send(currentComment, parentComment);
-            console.log("@ 了其他人, 已提醒:" + parentComment.get('mail'));
+            console.log("这条评论 @ 了其他人, 已提醒至对方邮箱:" + parentComment.get('mail'));
         } else {
-            console.log("@ 了其他人，但这个人没留邮箱... 无法通知");
+            console.log("这条 @ 了其他人， 但被 @ 的人没留邮箱... 无法通知");
         }
     }, function (error) {
         console.warn('好像 @ 了一个不存在的人!');
