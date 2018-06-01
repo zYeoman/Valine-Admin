e818d7277b1e41bb70189a64677e63007b834ff1
Yongwen Zhuang <zyeoman@163.com>
2018-06-01 13:01:22 +0800

    :ambulance: Fix Auto Notify

--- server.js
+++ server.js
@@ -26,16 +26,32 @@ query.find().then(function (results) {
         // 发送博主通知邮件
         mail.notice(currentComment);
         // AT评论通知
-        let rid = currentComment.get('rid');
-        if (!rid) {
-            console.log('没有@任何人，结束!');
+        let rid;
+
+        // 拿到评论内容
+        let comment = currentComment.get('comment');
+
+        // 判断是否包含 class="at", 如包含则表示 @ 了别人, 截取 @ 的邮箱 hash 值
+        if (comment.indexOf("class=\"at\"") != -1) {
+            let start = comment.indexOf("#") + 1;
+            let end = comment.substr(start).indexOf("'");
+            rid = comment.substr(start, end);
+        } else {
+            console.log("这条评论没有 @ 任何人");
             return;
         }
+
+        // 查 @ 的人的邮箱, 并发送邮件.
         let query = new AV.Query('Comment');
         query.get(rid).then(function (parentComment) {
-            mail.send(currentComment, parentComment);
+            if (parentComment.get('mail')) {
+                mail.send(currentComment, parentComment);
+                console.log("这条评论 @ 了其他人, 已提醒至对方邮箱:" + parentComment.get('mail'));
+            } else {
+                console.log("这条 @ 了其他人， 但被 @ 的人没留邮箱... 无法通知");
+            }
         }, function (error) {
-            console.warn('获取@对象失败！');
+            console.warn('好像 @ 了一个不存在的人!');
         });
     });
 }, function (err) {
